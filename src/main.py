37769fe30eeb05e9c743b5f258939c7a81a557e3
dariustb <dariustb@tech-center.com>
2022-11-26 17:41:37 -0500

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,15 @@
+# Python v. 3.10.4, 64-bit
+
+# ------------------------------------------------
+# This is to solve the challenge given by
+# Charles Cornell for some free music instruction
+# ------------------------------------------------
+
+
+def main():
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
