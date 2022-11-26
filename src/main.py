3eef3eeb8bd09604a7f019f5ce42e7b5cd2df46e
dariustb <dariustb@tech-center.com>
2022-11-26 17:55:53 -0500

    Added extractFrames

--- src/main.py
+++ src/main.py
@@ -5,11 +5,27 @@
 # Charles Cornell for some free music instruction
 # ------------------------------------------------
 
+import cv2      # Exracts frames from video
 
-def main():
+def extractFrames(video_path: str):
+    ''' Extracts frames from video_path and saves them into ouptut folder'''
+    # From fireant on StackOverflow
+    # https://stackoverflow.com/questions/33311153/python-extracting-and-saving-video-frames
+    
+    vidcap = cv2.VideoCapture(video_path)
+    success,image = vidcap.read()
+    count = 0
+    while success:
+        cv2.imwrite("output/frame%d.jpg" % count, image)     # save frame as JPEG file      
+        success,image = vidcap.read()
+        print('Read a new frame: ', success)
+        count += 1
+    print("Count =", count)
+
+def searchFrames():
     pass
 
-
-
 if __name__ == "__main__":
-    main()
+    video_path = "input/skunky.mp4"
+    
+    extractFrames(video_path)
