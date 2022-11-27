6776820420d8f447b15edf5e8205bc467c85b392
dariustb <dariustb@tech-center.com>
2022-11-27 03:42:01 -0500

    Small changes

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@
 from functions import run
 
 if __name__ == "__main__":
-    video_path = "input/vid.mp4"       # Video we are searching through
-    ref_path   = "input/ref.jpg"        # Image we are looking for in vid
+    video_path = "input/vid.mp4"    # Video we are searching through
+    ref_path   = "input/ref.jpg"    # Image we are looking for in vid
 
     run(video_path, ref_path)
 
